eabaf83f6820983c39cae9746ec736464ab35078
Stone-Monkey1 <92111289+Stone-Monkey1@users.noreply.github.com>
2026-02-16 10:00:58 -0500

    Add project files.

---
+++ LayEggs/Program.cs
@@ -0,0 +1,79 @@
+﻿while (true)
+{
+    Bird bird;
+    Console.Write("\nPress P for pigeon, O for ostrich: ");
+    char key = Char.ToUpper(Console.ReadKey().KeyChar);
+    if (key == 'P') bird = new Pigeon();
+    else if (key == 'O') bird = new Ostrich();
+    else return;
+    Console.Write("\nHow many eggs should it lay? ");
+    if (!int.TryParse(Console.ReadLine(), out int numberOfEggs)) return;
+    Egg[] eggs = bird.LayEggs(numberOfEggs);
+    foreach (Egg egg in eggs)
+    {
+        Console.WriteLine(egg.Description);
+    }
+}
+
+class Egg
+{
+    public double Size { get; private set; }
+    public string Color { get; private set; }
+    public Egg(double size, string color)
+    {
+        Size = size;
+        Color = color;
+    }
+    public string Description
+    {
+        get { return $"A {Size:0.00}cm {Color} egg"; }
+    }
+}
+
+class Bird
+{
+    public virtual Egg[] LayEggs(int numberOfEggs)
+    {
+        Console.Error.WriteLine("Bird.LayEggs should never get called");
+        return new Egg[0];
+    }
+}
+
+class Pigeon : Bird
+{
+
+    public override Egg[] LayEggs(int numberofEggs)
+    {
+        Egg[] eggs = new Egg[numberofEggs];
+        for (int i = 0; i < numberofEggs; i++)
+        {
+            if (Random.Shared.Next(4) == 0)
+            {
+                eggs[i] = new BrokenEgg("white");
+            }
+            else
+            {
+                eggs[i] = new Egg(Random.Shared.NextDouble() * 2 + 1, "white");
+            }
+        }
+        return eggs;
+    }
+}
+
+class Ostrich : Bird
+{
+    public override Egg[] LayEggs(int numberofEggs)
+    {
+        Egg[] eggs = new Egg[numberofEggs];
+        for (int i = 0; i < numberofEggs; i++)
+        {
+            eggs[i] = new Egg(Random.Shared.NextDouble() + 12, "speckled");
+        }
+        return eggs;
+    }
+}
+class BrokenEgg : Egg
+{
+    public BrokenEgg(string color) : base(0, $"broken {color}")
+    { Console.WriteLine("A bird laid a broken egg"); }
+}
